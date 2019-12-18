79a328a803cda9fb3f995e900f838f45cab5f06c
Zihao He <awayhosi@hotmail.com>
2019-12-18 17:47:01 +0100

    Reconstructed into lighter version.

--- src/goal_sequence_driver_server.py
+++ src/goal_sequence_driver_server.py
@@ -8,6 +8,7 @@ import actionlib
 import numpy as np
 
 from nav_msgs.msg import Odometry
+from actionlib_msgs.msg import GoalStatus
 from tf.transformations import quaternion_from_euler
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from neo_goal_sequence_driver.srv import command, commandResponse
@@ -23,37 +24,141 @@ dist = 0.0
 time_start = None
 former_pose = None
 smallest_step_length = 0.000002
-pkg_name = 'neo_goal_sequence_driver'
-switch = pkg_name+'/switch'
-occupied = pkg_name+'/occupied'
+
+switch = None
 
 # service option
 service_called = True
 
 if(not service_called):
-	class CMD:
-		switch = "up"
-	cmd_default = CMD()
-	rospy.set_param(occupied, False)
+	
+	switch = True
+	cmd_default = None
 
 # path to configuration files
 rospack = rospkg.RosPack()
+pkg_name = 'neo_goal_sequence_driver'
 goal_list = rospack.get_path(pkg_name)+'/config/goal_list.yaml'
 driver_config = rospack.get_path(pkg_name)+'/config/driver_config.yaml'
 
-# get server ready for service, including embedded odometer
-def goal_sequence_driver_server():
+# transformation of data: euler -> quaternion
+def create_quaternion(goal_num, mat):
 
-	global goal_list
-	global driver_config
+	# read position & orientation(euler) from matrix and transfer them
+	x = mat[goal_num][0]
+	y = mat[goal_num][1]
+	theta = np.radians(mat[goal_num][2])
+	quaternion = quaternion_from_euler(0, 0, theta)
+	return [x, y, quaternion]
 
-	rospy.init_node('goal_sequence_driver_server')
-	server = rospy.Service('goal_sequence_driver_run', command, goal_sequence_driver_run)
-	server_stop = rospy.Service('goal_sequence_driver_stop', command, goal_sequence_driver_stop)
-	rospy.loginfo("goal_sequence_driver_server is up.")
-	odom_sub = rospy.Subscriber("odom", Odometry, odom_callback)
-	rospy.loginfo("Odometer is up.")
-	rospy.spin()
+# creat goal structure for move_base node
+def create_goal(pose):
+
+	# create goal from position & orientation(quaternion)
+	goal = MoveBaseGoal()
+	goal.target_pose.header.frame_id = "map"
+	goal.target_pose.header.stamp = rospy.Time.now()
+	goal.target_pose.pose.position.x = pose[0]
+	goal.target_pose.pose.position.y = pose[1]
+	goal.target_pose.pose.orientation.z = pose[2][2]
+	goal.target_pose.pose.orientation.w = pose[2][3]
+	return goal
+
+# callback function of odom subscriber
+def odom_callback(odometry):
+
+	global dist
+	global former_pose
+	global smallest_step_length
+
+	pose = odometry.pose.pose.position
+	if(former_pose == None):
+
+		former_pose = pose
+	else:
+
+		d_dist = np.sqrt((pose.x - former_pose.x)**2 + (pose.y - former_pose.y)**2)
+		former_pose = pose
+		if(d_dist < smallest_step_length):
+			d_dist = 0
+		dist += d_dist
+
+# callback function of service run
+def goal_sequence_driver_run(cmd):
+
+	global mat
+	global switch
+
+	rospy.loginfo("goal_sequence_driver up.")
+	switch = True
+	goal_sequence_driver()
+	return "Service done."
+
+# callback function of service stop
+def goal_sequence_driver_stop(cmd):
+
+	global switch
+	# stop the SimpleActionClient
+	if(switch):
+		switch = False
+	else:
+		return "goal_sequence_driver is not running."
+	return "Shut down."
+	
+# using actionlib to move robot around
+def goal_sequence_driver():
+
+	global switch
+	global patience
+
+	client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+	current_task_start = rospy.get_time()
+	goal_status = GoalStatus()
+	if(client.wait_for_server()):
+
+		rospy.loginfo("Action client server up.")
+	i = 0
+	while(not rospy.is_shutdown() and switch):
+
+		try:
+
+			pose = create_quaternion(i, mat)
+		except:
+			if(i != 0):
+
+				rospy.loginfo("All goals reached, sequence drive task completed.")
+
+			else:
+
+				rospy.loginfo("Process died of unknown reason.")
+				return "Service failed."
+			if(not infi_param):
+
+				break
+			else:
+
+				i = 0
+				pose = create_quaternion(i, mat)
+		goal = create_goal(pose)
+		client.send_goal(goal)
+		while(not client.get_state() == goal_status.SUCCEEDED):
+			pass
+
+		rospy.loginfo("Goal reached: "+str(mat[i]))
+		if(print_option == "all" or print_option == "local"):
+
+			rospy.loginfo("Current task duration:"+str(rospy.get_time() - current_task_start)+"s")						
+			time.sleep(1)
+		else:
+
+			rospy.loginfo("Out of patience, moving on to next!")
+		i += 1
+	if(print_option == "all" or print_option == "total"):
+
+		rospy.loginfo("Total distance traveled is:"+str(dist)+"m")
+		rospy.loginfo("Total time traveled is:"+str(rospy.get_time() - time_start)+"s")
+	switch = False
+	return 0
 
 # fetch data from .yaml files
 def load_from_yaml():
@@ -109,143 +214,26 @@ def load_from_yaml():
 
 mat = load_from_yaml()
 
-# transformation of data: euler -> quaternion
-def create_quaternion(goal_num, mat):
-
-	# read position & orientation(euler) from matrix and transfer them
-	x = mat[goal_num][0]
-	y = mat[goal_num][1]
-	theta = np.radians(mat[goal_num][2])
-	quaternion = quaternion_from_euler(0, 0, theta)
-	return [x, y, quaternion]
-
-# creat goal structure for move_base node
-def create_goal(pose):
-
-	# create goal from position & orientation(quaternion)
-	goal = MoveBaseGoal()
-	goal.target_pose.header.frame_id = "map"
-	goal.target_pose.header.stamp = rospy.Time.now()
-	goal.target_pose.pose.position.x = pose[0]
-	goal.target_pose.pose.position.y = pose[1]
-	goal.target_pose.pose.orientation.z = pose[2][2]
-	goal.target_pose.pose.orientation.w = pose[2][3]
-	return goal
-
-# callback function of odom subscriber
-def odom_callback(odometry):
-
-	global dist
-	global time_start
-	global former_pose
-	global smallest_step_length
-
-	pose = odometry.pose.pose.position
-	if(former_pose == None):
-
-		former_pose = pose
-		time_start = rospy.get_time()
-	else:
-
-		d_dist = np.sqrt((pose.x - former_pose.x)**2 + (pose.y - former_pose.y)**2)
-		former_pose = pose
-		if(d_dist < smallest_step_length):
-			d_dist = 0
-		dist += d_dist
-
-# callback function of service run
-def goal_sequence_driver_run(cmd):
-
-	global mat
-
-	rospy.loginfo("goal_sequence_driver up.")
-	rospy.set_param(switch, True)	
-	try:
-	
-		rospy.get_param(occupied)
-	except:
-
-		rospy.set_param(occupied, False)
-	if(not rospy.get_param(occupied)):
-		
-		rospy.set_param(occupied, True)
-		client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-		current_task_start = rospy.get_time()
-		if(client.wait_for_server()):
-
-			rospy.loginfo("Client server up.")
-		i = 0
-		#mat = load_from_yaml()
-		while(not rospy.is_shutdown() and rospy.get_param(switch)):
-
-			try:
-
-				pose = create_quaternion(i, mat)
-			except:
-				if(i != 0):
-
-					rospy.loginfo("All goals checked, sequence drive task completed.")
-
-				else:
-
-					rospy.loginfo("Process died of unknown reason.")
-					rospy.set_param(occupied, False)
-					return "Service failed."
-				if(infi_param != True):
-
-					break
-				else:
-
-					i = 0
-					pose = create_quaternion(i, mat)
-			goal = create_goal(pose)
-			client.send_goal(goal)
-			# for test
-			rospy.loginfo("Goal sent to action server.")
-			while(not rospy.is_shutdown()):
-
-				if(client.wait_for_result(rospy.Duration.from_sec(patience)) == True):
-
-					rospy.loginfo("Goal reached: "+str(mat[i]))
-					if(print_option == "all" or print_option == "local"):
-
-						rospy.loginfo("Current task duration:"+str(rospy.get_time() - current_task_start)+"s")						
-					time.sleep(1)
-					break
-				else:
-					# for test
-					print(patience)
-					rospy.loginfo("Out of patience, moving on to next!")
-					break
-			i += 1
-		rospy.set_param(occupied, False)
-		if(print_option == "all" or print_option == "total"):
-
-			rospy.loginfo("Total distance traveled is:"+str(dist)+"m")
-			rospy.loginfo("Total time traveled is:"+str(rospy.get_time() - time_start)+"s")
-	return "Service done."
-
-# callback function of service stop
-def goal_sequence_driver_stop(cmd):
-
-	rospy.loginfo("goal_sequence_driver down.")
-	rospy.set_param(switch, False)
-
-	return "Shut down."
-
-
 if __name__ == '__main__':
 
+	global time_start
+	rospy.init_node('goal_sequence_driver')
+	
+	print(time_start)
 	if(service_called):
 
-		goal_sequence_driver_server()
-		if(rospy.is_shutdown()):
-			rospy.set_param(occupied, False)
+		server = rospy.Service('goal_sequence_driver_run', command, goal_sequence_driver_run)
+		server_stop = rospy.Service('goal_sequence_driver_stop', command, goal_sequence_driver_stop)
+		rospy.loginfo("goal_sequence_driver server is up.")
+		odom_sub = rospy.Subscriber("odom", Odometry, odom_callback)
+		rospy.loginfo("Odometer is up.")
+		time_start = rospy.get_time()
+		rospy.spin()
 
 	else:
 
-		rospy.init_node('goal_sequence_driver')
 		rospy.loginfo("goal_sequence_driver is not responding to any request.")
 		odom_sub = rospy.Subscriber("odom", Odometry, odom_callback)
 		rospy.loginfo("Odometer is up.")
+		time_start = rospy.get_time()
 		goal_sequence_driver_run(cmd_default)
--- src/goal_sequence_visualizer.py
+++ src/goal_sequence_visualizer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import time
 import rospy
 import numpy as np
 import goal_sequence_driver_server as server
@@ -7,9 +8,8 @@ import goal_sequence_driver_server as server
 from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import Pose, PoseArray, Quaternion
 
-
 # visualizer of goals in form of axes
-def goal_sequence_visualizer(mat):
+def init_goal_sequence_visualizer(mat):
 
 	rospy.init_node('neo_goal_sequence_visualizer')
 	rate = rospy.Rate(5)
@@ -28,20 +28,14 @@ def goal_sequence_visualizer(mat):
 		pose.orientation.z = quaternion[2]
 		pose.orientation.w = quaternion[3]
 		pose_array.poses.append(pose)
-
-	try:
-
-		oc = rospy.get_param(server.occupied)
-		while(oc):
-
-			pub.publish(pose_array)
-			rate.sleep()
-	except:
-
-		pass
-
+	#	pub.publish(pose_array)
+	#	rate.sleep()
+	return pub, rate, pose_array
 
 if __name__ == '__main__':
 
+	pub, rate, pose_array =	init_goal_sequence_visualizer(server.mat)
 	while(not rospy.is_shutdown()):
-		goal_sequence_visualizer(server.mat)
+
+		pub.publish(pose_array)
+		rate.sleep()
