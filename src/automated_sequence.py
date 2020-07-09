946df64125717576bf1e5214549bcfc91eb724bc
pradheep <padhupradheep@gmail.com>
2020-07-09 11:35:41 +0200

    automated goal sequnce functionality is ready to be in use

--- src/automated_sequence.py
+++ src/automated_sequence.py
@@ -1,55 +1,53 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-# from move_base_simple_nsgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Pose, PoseStamped, PoseArray
-from visualization_msgs.msg import Marker, MarkerArray
 from tf.transformations import quaternion_from_euler
 import roslaunch
 import os
 import yaml
 import rospkg
 
-
+# Accessing the goal list
 rospack = rospkg.RosPack()
 pkg_name = 'neo_goal_sequence_driver'
 goal_list = rospack.get_path(pkg_name)+'/config/goal_list.yaml'
 
 
 class AutomatedGoal:
-
-	def __init__(self):
+# Class initialization
+	def __init__(self, n_poses):
 		self.goal = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.GoalCB)
 		self.goal_list = []
 		self.pose = Pose()
 		self.pose_array = PoseArray()
 		self.pose_array.header.seq = 1
 		self.pose_array.header.frame_id = "/map"
-		# self.marker_array = MarkerArray()
 		self.pub_marker = rospy.Publisher('pose_array', PoseArray, queue_size=1)
 		self.launch = False
+		self.n_poses = n_poses # Support variable for publishing the pose arrays
+		self.n_goals = n_poses # Support variable for logging
 
 	def GoalCB(self,data):
 		self.goal_list.append([data])
-		if(len(self.goal_list)==3):
-			rospy.loginfo("Let's publish the goals")
+		if(len(self.goal_list)==self.n_poses):
+			rospy.loginfo("Let's visualize the goals and initialize the goals for goal sequence driver ")
 			for i in range(0, len(self.goal_list)):
-				# print(self.goal_list[i][0].pose.position.x)
 				self.pose = Pose()
 				self.pose.position.x = self.goal_list[i][0].pose.position.x
 				self.pose.position.y = self.goal_list[i][0].pose.position.y
-				self.pose.position.z = 0
-				quaternion = quaternion_from_euler(0, 0, np.radians(self.goal_list[i][0].pose.orientation.z))
-				self.pose.orientation.x = quaternion[0]
-				self.pose.orientation.y = quaternion[1]
-				self.pose.orientation.z = quaternion[2]
-				self.pose.orientation.w = quaternion[3]
+				self.pose.position.z = self.goal_list[i][0].pose.orientation.z
+				self.pose.orientation.x = self.goal_list[i][0].pose.orientation.x
+				self.pose.orientation.y = self.goal_list[i][0].pose.orientation.y
+				self.pose.orientation.z = self.goal_list[i][0].pose.orientation.z
+				self.pose.orientation.w = self.goal_list[i][0].pose.orientation.w
 				self.pose_array.poses.append(self.pose)
 			self.pub_marker.publish(self.pose_array)
 			self.launch = True
 
 		else:
-			rospy.loginfo("i goals are left")
+			rospy.loginfo("%i goals are left", self.n_goals-1)
+			self.n_goals = self.n_goals-1
 
 		if(self.launch):
 			data = [{'X': self.goal_list[0][0].pose.position.x, 'Y': self.goal_list[0][0].pose.position.y, 'theta': self.goal_list[0][0].pose.orientation.z}, \
@@ -58,21 +56,14 @@ class AutomatedGoal:
 			with open(goal_list, 'w') as outfile:
 				yaml.dump(data, outfile, default_flow_style=False)
 			os.system("roslaunch neo_simulation mpo_500_move_base.launch")
-			# os.system("roslaunch neo_goal_sequence_driver neo_goal_sequence_driver.launch")
 
-		# if(self.launch):
-		# 	uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-		# 	roslaunch.configure_logging(uuid)
-		# 	launch = roslaunch.parent.ROSLaunchParent(uuid, [file_path])
-		# 	launch.start()
 
 if __name__ == '__main__':
 	rospy.init_node('Automated_goal')
-	ag = AutomatedGoal()
-	rate = rospy.Rate(0.5)
-	rospy.loginfo("Please select minimum of 3 poses")
+	n_poses = 4 # Set number of goals you want to give for the automated process! 
+	ag = AutomatedGoal(n_poses)
+	rate = rospy.Rate(0.01)
+	rospy.loginfo("Please select %i poses", n_poses)
+	rospy.loginfo("%i goals are left", n_poses)
 	while(not rospy.is_shutdown()):
 		rate.sleep()
-
-
-	# goal = rospy.Subscriber("/move_base/goal", MoveBaseGoal, GoalCB)
